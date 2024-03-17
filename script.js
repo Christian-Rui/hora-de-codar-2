20bc42c6ac5362c81390afb5934380c8ce100489
Christian Rui <chris2w0social@gmail.com>
2024-03-17 03:32:40 -0300

    Adicionando descrição das atividades em comentarios

--- script.js
+++ script.js
@@ -1,34 +1,59 @@
 // Atividade 1
+//Escreva um programa em que o usuário informe dois números utilizando o "prompt". Então escreva em tela o
+//maior deles.
 ArmazenarMaiorNumero(2, 1);
 
 // Atividade 2
+//Faça um programa que leia um valor informado pelo usuário e diga se o valor informado é positivo, negativo
+//ou zero.
 VerificarSinalNumero()
 
 // Atividade 3
+//Faça um programa para ler 3 valores (considere que não serão informados valores iguais) e escrever o maior
+//deles.
 ArmazenarMaiorNumero(3, 1);
 
 // Atividade 4
+//Faça um programa que leia 3 valores informados pelo usuário (considere que não serão informados valores
+//iguais) e escrever a soma dos 2 maiores.
 ArmazenarMaiorNumero(3, 2);
 
 // Atividade 5
+//Faça um programa que leia 6 valores informados pelo usuário, calcule, exiba os números informados e escreva
+//a média aritmética desses valores lidos.
 ArmazenarMedia(6);
 
 // Atividade 6
+//Faça um programa que receba quatro valores informados pelo usuário, mas informe somente o primeiro, o
+//último e o maior de todos eles (considere que todos os números informados serão diferentes)
 ArmazenarMaiorNumero(4, 3);
 
 // Atividade 7
+//Faça um programa que leia 6 números que o usuário vai informar. Todos os números lidos com valor inferior a
+//72 devem ser somados. Escreva o valor final da soma efetuada e também todos valores que o usuário informou.  
 SomarNumeros(6);
 
 // Atividade 8
+//Escreva um programa que calcule a média de quatro números informados pelo usuário, mas somente se esses
+//números forem maiores que 0 e menores que 10. No final, se a média for maior que cinco o usuário receberá uma
+//mensagem "Você passou no teste". Em qualquer outra situação, ele receberá uma mensagem de "tente novamente"
 CalcularMediaDez(4);
 
 // Atividade 9
+//Escreva um programa para ler o ano de nascimento de uma pessoa e escrever uma mensagem que diga se ela
+//poderá ou não votar este ano (não é necessário considerar o mês em que ela nasceu).
 VerificarSePodeVotar()
 
 // Atividade 10
+//Tendo como entrada a altura e o sexo (codificado da seguinte forma: 1: feminino 2: masculino) de uma
+//pessoa, construa um programa que calcule e imprima seu peso ideal, utilizando as seguintes fórmulas:
 CalcularIMC();
 
 // Atividade 11
+//Uma micro calculadora
+//Escreva um programa para ler 2 valores inteiros informados pelo usuário e uma das seguintes operações a serem
+//executadas (codificada da seguinte forma: 1. Adição, 2. Subtração, 3. Divisão, 4. Multiplicação).
+//O programa deve calcular e escrever o resultado dessa operação sobre os dois valores lidos. 
 Calculadora(2);
 
 
